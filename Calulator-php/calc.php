cc7eea84661f72a318626446df03a16ee239408d
mr-aman-git <amanazr2003@gmail.com>
2025-02-11 10:28:03 +0530

    design added

---
+++ Calulator-php/calc.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="calc.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <div class="main">
+            <div class="main-box">
+                <h2>Calculator</h2>
+                <form>
+                    <div class="form-box">
+                        <input type="number" name="text1" placeholder="Enter Number">
+                    </div>
+
+                    <div class="form-select">
+                        <select name="operation" id="">
+                            <option value="add">+</option>
+                            <option value="sub">-</option>
+                            <option value="mult">*</option>
+                        </select>
+                    </div>
+
+                    <div class="form-box">
+                        <input type="number" name="text2" placeholder="Enter Number">
+                    </div>
+
+                    <button name="submit">Submit</button>
+                    
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
